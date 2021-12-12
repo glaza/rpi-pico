067174edbf9441cf68607188f0b906ca82d9c02b
glaza <33300759+glaza@users.noreply.github.com>
2021-12-12 14:33:29 -0500

    Rearrange buttons

--- MacroKeyboard/code.py
+++ MacroKeyboard/code.py
@@ -1,11 +1,13 @@
-# Sample application: https://learn.adafruit.com/circuitpython-essentials/circuitpython-hid-keyboard-and-mouse
-# API Reference: https://circuitpython.readthedocs.io/projects/hid/en/latest/api.html#adafruit-hid-keycode-keycode
+# Sample application:
+# https://learn.adafruit.com/circuitpython-essentials/circuitpython-hid-keyboard-and-mouse
+# API Reference:
+# https://circuitpython.readthedocs.io/projects/hid/en/latest/api.html#adafruit-hid-keycode-keycode
 
 import time
 import board
 from adafruit_hid.keycode import Keycode
 
-from utils import send, sequence, write
+from utils import noOp, send, write
 from button import Button
 from encoder import Encoder
 
@@ -15,8 +17,8 @@ buttons = [
     Encoder(  # Left Encoder
         "IDEA Debugging",
         board.GP4, board.GP5,
-        send(Keycode.SHIFT, Keycode.F8), # Step Out of
-        send(Keycode.F8)), # Step Over
+        send(Keycode.SHIFT, Keycode.F8),  # Step Out of
+        send(Keycode.F8)),  # Step Over
     Encoder(  # Middle Encoder
         "Linux Change Workspaces",
         board.GP2, board.GP3,
@@ -31,7 +33,8 @@ buttons = [
     Button(  # Left Encoder
         "IDEA Debugging Step Into",
         board.GP6,
-        send(Keycode.F7)), # Step Into
+        send(Keycode.F7)),  # Step Into
+    Button(  # Middle Encoder
         "Linux Expose",
         board.GP7,
         send(Keycode.COMMAND)),
@@ -45,54 +48,58 @@ buttons = [
         board.GP8,
         send(Keycode.CONTROL, Keycode.F5)),
     Button(  # Green
-        "Linux Lock Screen",
+        "IDEA Comment",
         board.GP9,
-        send(Keycode.COMMAND, Keycode.L)),
+        send(Keycode.CONTROL, Keycode.FORWARD_SLASH)),
     Button(  # White
-        "Short only",
+        "Linux Lock Screen",
         board.GP10,
-        send(Keycode.F5)),
+        send(Keycode.COMMAND, Keycode.L)),
 
     Button(  # Brown
         "IDEA Rename",
         board.GP11,
         send(Keycode.SHIFT, Keycode.F6)),
     Button(  # Clear
-        "IDEA Comment",
+        "IDEA Go to Implementation/File Structure",
         board.GP12,
-        send(Keycode.CONTROL, Keycode.FORWARD_SLASH)),
+        noOp,  # Down
+        send(Keycode.CONTROL, Keycode.ALT, Keycode.B),  # Short
+        send(Keycode.CONTROL, Keycode.F12)),  # Long
     Button(  # Grey
         "Linux Screengrab",
         board.GP13,
         send(Keycode.SHIFT, Keycode.CONTROL, Keycode.PRINT_SCREEN)),
 
     Button(  # Red
-        "IDEA Extract Constant",
+        "IDEA Extract Constant/Method",
         board.GP14,
-        send(Keycode.COMMAND, Keycode.CONTROL, Keycode.C)),
+        noOp,  # Down
+        send(Keycode.CONTROL, Keycode.ALT, Keycode.C),  # Short
+        send(Keycode.CONTROL, Keycode.ALT, Keycode.M)),  # Long
     Button(  # Silent Red
         "IDEA Inline",
         board.GP15,
-        send(Keycode.COMMAND, Keycode.CONTROL, Keycode.N)),
+        send(Keycode.CONTROL, Keycode.ALT, Keycode.N)),
     Button(  # Speed Silver
-        "Slack Happy Face",
+        "Copy-Paste",
         board.GP16,
-        write(":) "),
-        send(Keycode.COMMAND, Keycode.ENTER)),
+        send(Keycode.CONTROL, Keycode.C),
+        send(Keycode.CONTROL, Keycode.V)),
 
     Button(  # Black
         "IDEA Organize Imports",
         board.GP17,
-        send(Keycode.COMMAND, Keycode.CONTROL, Keycode.O)),
+        send(Keycode.CONTROL, Keycode.ALT, Keycode.O)),
     Button(  # Silent Black
         "IDEA Reformat Code",
         board.GP18,
-        send(Keycode.COMMAND, Keycode.CONTROL, Keycode.L)),
+        send(Keycode.CONTROL, Keycode.ALT, Keycode.L)),
     Button(  # RGB Silver
         "Slack Thumbs Up",
         board.GP19,
         write(":+1: "),
-        send(Keycode.COMMAND, Keycode.ENTER))
+        send(Keycode.CONTROL, Keycode.ENTER))
 ]
 
 while True:
